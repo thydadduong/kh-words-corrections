b69089ec188a4894967ebb4727a8ff0a5acdbe01
thydaduong <thydaduong7@gmail.com>
2019-07-15 18:15:07 +0700

    completed checking  and highlight incorrect words

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,46 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <nav id="nav" class="navbar sticky-top navbar-expand-md navbar-light shadow bg-light">
+      <div class="container">
+        <a class="navbar-brand" href="#">Khmer Words Correction</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <router-link to="/" class="nav-link">Home</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/about" class="nav-link">About</router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <br/>
+    <div class="container">
+      <router-view />
     </div>
-    <router-view/>
   </div>
 </template>
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 #nav {
-  padding: 30px;
   a {
     font-weight: bold;
     color: #2c3e50;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,139 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="card mb-4">
+      <div class="card-header">
+        <!-- eslint-disable-next-line -->
+        <p class="card-text">បញ្ចូលឃ្លា ឬ​ប្រយោគៈ</p>
+      </div>
+      <div class="card-body">
+        <!-- eslint-disable-next-line -->
+        <!-- placeholder="បញ្ចូល​ពាក្យ​ត្រង់នេះ..." -->
+        <textarea rows="5" class="form-control" v-model="editingWords" />
+      </div>
+      <div class="card-footer">
+        <div class="btn-group" role="group" aria-label="Basic example">
+          <button type="button" class="btn btn-outline-dark" @click="findIncorrectWords">
+            <!-- eslint-disable-next-line -->
+            ពិនិត្រ​អក្ខរាវិរុទ្ធ
+          </button>
+          <!-- eslint-disable-next-line -->
+          <button type="button" class="btn btn-outline-dark" @click="resetTextFormat">ជម្រះ</button>
+          <!-- eslint-disable-next-line -->
+          <button type="button" class="btn btn-outline-dark">ដាក់ ZWSP</button>
+        </div>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-header">លទ្ធផល</div>
+      <div class="card-body">
+        <div id="word-preview">
+          {{splitedWord}}
+          <div v-html="checkedWords" />
+          <!-- <div v-html="findIncorrectWords()"></div> -->
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  name: "home",
+  components: {},
+  data() {
+    return {
+      editingWords:
+        "មួយ​ប៉​ព្រិច​ភ្នែក​សោះ​ ពេលនេះ​អ្នក​បាន​ដើរ​ដល់​ចំណុច​កណ្តាល​នៃ​ផ្លូវ​ហើយ​ នៅសល់​ពាក់កណ្តាល​ទៀត​អ្នក​អាច​ត្រឡប់ទៅវិញ​ហើយ​។​ មួយឆ្នាំ​អាច​ថា​លឿន​ តែ​ក៏អាច​ជា​យឺត​ដែរ​។ មួយឆ្នាំ​កន្លង​នេះ​មានរឿង​ជាច្រើន​ដែល​បាន​កើតឡើង​ មាន​ទាំង​ល្អ​ទាំង​អាក្រក់​បញ្ហា​មិនមែន​សុទ្ធតែ​អាក្រក់​នោះ​ទេ​ តែបើ​យើង​អាច​រក​វិធី​ដោះ​ស្រាយ​បាន​វា​នឹង​ផ្តល់​បទ​ពិសោធ​ន៍​ល្អ​ៗ​ដល់​យើង​។​ ចំណាំ​៖ តែបើ​បញ្ហា​កើត​ច្រើនពេក​ក៏​មិន​ចូលចិត្ត​ដែរ",
+      khWords: "",
+      missignWords: [],
+      checkedWords: "",
+      splitedWord: []
+    };
+  },
+  methods: {
+    findIncorrectWords() {
+      // if (!this.query) {
+      //   return this.khWords;
+      // }
+      // return this.khWords.replace(new RegExp(this.query, "gi"), match => {
+      //   return '<span class="highlightText">' + match + "</span>";
+      // });
+      const wordsToCheck = this.splitWord() || [];
+      this.khWords = this.editingWords;
+      this.splitedWord = wordsToCheck;
+      // this.khWords= wordsToCheck
+      wordsToCheck.forEach(word => {
+        let firstChar = word.charAt(0);
+        try {
+          let wordsToCompare = require("../assets/group_words/" +
+            firstChar +
+            ".json");
+          if (!wordsToCompare.includes(word)) {
+            this.missignWords.push(word);
+          }
+        } catch (error) {
+          let wordsToCompare = require("../assets/group_words/others.json");
+          if (!wordsToCompare.includes(word)) {
+            this.missignWords.push(word);
+          }
+        }
+      });
+      if (this.missignWords.length) {
+        this.checkedWords = this.applyErrorFormat();
+      } else {
+        this.checkedWords = "";
+      }
+    },
+    applyErrorFormat() {
+      let errorRegex = "";
+      this.missignWords.forEach(word => {
+        errorRegex += word + "|";
+      });
+      errorRegex = errorRegex.slice(0, -1);
+      return this.khWords.replace(new RegExp(errorRegex, "gu"), match => {
+        console.log("math: ", match);
+
+        return '<span class="highlightText">' + match + "</span>";
+      });
+    },
+    splitWord() {
+      const words = this.editingWords
+        .replace(/\s+/g, "\u200B")
+        .replace(/\u200B+/g, "\u200B")
+        .split("\u200B");
+      const uniqueWords = words.filter(
+        (val, i, self) => self.indexOf(val) === i
+      );
+
+      return uniqueWords;
+    },
+    resetTextFormat() {
+      this.checkedWords = "";
+    }
   }
-}
+  // computed: {
+  //   splitWord() {
+  //     const words = this.editingWords
+  //       .replace(/\s+/g, "\u200B")
+  //       .replace(/\u200B+/g, "\u200B")
+  //       .split("\u200B");
+  //     const uniqueWords = words.filter(
+  //       (val, i, self) => self.indexOf(val) === i
+  //     );
+  //     return uniqueWords;
+  //   }
+  // }
+};
 </script>
+
+<style lang="scss">
+#word-preview {
+  min-height: 14rem;
+}
+
+.highlightText {
+  background: yellow;
+}
+</style>
+
